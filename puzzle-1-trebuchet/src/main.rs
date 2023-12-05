3923325260db68654e02b7e1c018c12f00826a3f
cetmoe <cetmoe>
2023-12-05 12:28:59 +0100

    not pretty but it works

--- puzzle-1-trebuchet/src/main.rs
+++ puzzle-1-trebuchet/src/main.rs
@@ -1,46 +1,75 @@
-use std::fs;
+use regex::{Regex, RegexSet};
+use std::{collections::HashMap, fs, os::unix::prelude::FileExt};
 
 fn main() {
     let contents = fs::read_to_string("input.txt").unwrap();
     let lines = contents.trim().split("\n");
 
-    let dict = [
-        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    let patterns = vec![
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "0", "1",
+        "2", "3", "4", "5", "6", "7", "8", "9",
     ];
 
+    let re = Regex::new(r"zero|one|two|three|four|five|six|seven|eight|nine|0|1|2|3|4|5|6|7|8|9")
+        .unwrap();
+
+    let shortest = patterns
+        .iter()
+        .fold(25, |acc, s| if s.len() < acc { s.len() } else { acc });
+
+    let longest = patterns
+        .iter()
+        .fold(0, |acc, s| if s.len() > acc { s.len() } else { acc });
+
     let sum = lines.fold(0, |mut acc, line| {
-        let chars_in_line = line.chars();
+        let mut strs: Vec<&str> = Vec::new();
+        for (ind, _) in line.chars().enumerate() {
+            for i in shortest..=longest {
+                let end = if ind + i > line.len() {
+                    line.len()
+                } else {
+                    ind + i
+                };
 
-        let mut number = String::new();
+                let x: Vec<_> = re
+                    .find_iter(&line[ind..end])
+                    .map(|m| &line[ind..end][m.start()..m.end()])
+                    .collect();
 
-        let left = chars_in_line
-            .clone()
-            .enumerate()
-            .filter(|(idx, c)| c.is_numeric() || idx == &chars_in_line.clone().count())
-            .map(|(idx, _)| idx)
-            .collect::<Vec<usize>>()[0];
-
-        let right = chars_in_line
-            .clone()
-            .rev()
-            .enumerate()
-            .filter(|(idx, c)| c.is_numeric() || idx == &chars_in_line.clone().count())
-            .map(|(idx, _)| idx)
-            .collect::<Vec<usize>>()[0];
-
-        for (i, c) in chars_in_line.clone().enumerate() {
-            dict.iter().enumerate().for_each(|(int, word)| {
-                if line[0..i].contains(word) {
-                    number = String::from(format!("{}", int));
-                } else if c.is_numeric() {
-                    number = String::from(format!("{}", c));
-                }
-            })
+                x.iter().for_each(|s| {
+                    strs.push(s);
+                });
+            }
         }
+        let number = format!(
+            "{}{}",
+            str_to_numeric(strs.first().unwrap()),
+            str_to_numeric(strs.last().unwrap())
+        );
 
         acc += number.parse::<u32>().unwrap();
         acc
     });
-
     println!("{}", sum);
+}
+
+fn str_to_numeric(input: &str) -> &str {
+    let matches = HashMap::from([
+        ("zero", "0"),
+        ("one", "1"),
+        ("two", "2"),
+        ("three", "3"),
+        ("four", "4"),
+        ("five", "5"),
+        ("six", "6"),
+        ("seven", "7"),
+        ("eight", "8"),
+        ("nine", "9"),
+    ]);
+
+    if input.len() == 1 {
+        input
+    } else {
+        matches.get(input).unwrap()
+    }
 }
