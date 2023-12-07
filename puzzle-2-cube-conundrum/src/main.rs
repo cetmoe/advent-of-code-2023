1afd25b7b529d577e0c87702b779baf9be08126d
cetmoe <cetmoe>
2023-12-07 10:30:41 +0100

    Added solution for day2p1

--- puzzle-2-cube-conundrum/src/main.rs
+++ puzzle-2-cube-conundrum/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    include_str!("../input.txt");
+    println!(
+        "{}",
+        include_str!("../input.txt")
+            .trim()
+            .split("\n")
+            .enumerate()
+            .filter_map(|(id, line)| {
+                line.split(":")
+                    .nth(1)
+                    .unwrap()
+                    .split(";")
+                    .all(|random_selection| {
+                        random_selection.split(",").all(|selection| {
+                            let (number, color) = selection.trim().split_once(" ").unwrap();
+                            match (number.parse::<usize>().unwrap(), color) {
+                                (0..=12, "red") => true,
+                                (0..=13, "green") => true,
+                                (0..=14, "blue") => true,
+                                (_, _) => false,
+                            }
+                        })
+                    })
+                    .then_some(id + 1)
+            })
+            .sum::<usize>()
+    );
 }
